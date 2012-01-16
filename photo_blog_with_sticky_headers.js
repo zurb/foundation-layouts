27ee7d225e8f21eded13af3bac6c773de78fd2a9
Tanya <tanya@zurb.com>
2012-01-16 11:31:47 -0800

    Adding layout 'photo blog with sticky headers'

---
+++ photo_blog_with_sticky_headers.js
@@ -0,0 +1,19 @@
+/* Foundation v2.1.4 http://foundation.zurb.com */
+$(document).ready(function () {
+	
+	$(window).scroll(function() {
+		var scroll = Math.max($('body').scrollTop(), $('html,body').scrollTop());
+		
+		$(".showcase .title").each(function(index, element) {
+			var slide = $(this).closest("li");
+			var top = slide.offset().top
+			var bottom = top + slide.outerHeight();
+			
+			if (scroll > top && scroll < (bottom - $(this).outerHeight()))
+				$(this).addClass("fixed");
+			else $(this).removeClass("fixed");
+		});
+		
+	});
+
+});
